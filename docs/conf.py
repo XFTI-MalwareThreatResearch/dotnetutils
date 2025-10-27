7cc3c4df23363c4104d1189921421bf726f3ebf0
unknown <Aaron.Gdanski@ibm.com>
2025-10-27 09:40:35 -0400

    Attempt 2 at gen docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,11 @@ author = 'IBM'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints"
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
